98c5e124ca432d05c86c79a2eb465a992ecf31dd
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2021-03-21 17:24:41 +0800

    Made multiple drag work

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ class Dog:
         self.win.geometry('1000x650')
         self.puppy = ImageTk.PhotoImage(Image.open('images/puppy.png').resize((80, 50), Image.ANTIALIAS))
         self.puppy_button = tk.Button(self.win, text = "Create Puppies", command = self.create_label)
-        self.puppy_label = tk.Label(self.win, image = self.puppy)
         self.puppy_button.pack()
         self.label_list = []
 
@@ -16,19 +15,16 @@ class Dog:
         print (e.x , e.y)
         x = self.win.winfo_pointerx() - self.win.winfo_rootx()
         y = self.win.winfo_pointery() - self.win.winfo_rooty()
-        self.puppy_label.place(height = 80, width = 50, x=x,y=y,anchor='center')
+        e.widget.place(height = 80, width = 50, x=x,y=y,anchor='center')
         print(' X : '+ str(e.x) +' and '+ 'Y : ' + str(e.y))
 
     def create_label(self):
-        self.puppy_label = tk.Label(self.win, image = self.puppy)
+        self.puppy_label = tk.Label(self.win, image = self.puppy, text = str(random.randint(1,3)))
         self.puppy_label.place(height = 80, width = 50, x= random.randint(300,400), y = random.randint(300, 400))
         self.label_list.append(self.puppy_label)
+        print("All the labels : ", self.label_list)
         self.puppy_label.bind('<B1-Motion>', self.move)
         return
-        
-    def bind_func(self):
-        for i in self.label_list:
-            i.bind('<B1-Motion>', self.move)
     
 lab = Dog()
 lab.win.mainloop()
