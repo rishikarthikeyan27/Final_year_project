f50e4fef438c3de0db9703737ac22b8d0a506bd7
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2021-03-22 19:02:50 +0800

    Add input works perfectly

--- test.py
+++ test.py
@@ -14,7 +14,6 @@ class Dog:
         self.dal = ImageTk.PhotoImage(Image.open('images/bin.png').resize((80, 50), Image.ANTIALIAS))
         self.dal_button = tk.Button(self.win, text = 'button', command = self.delete_all)
         self.dal_button.place(width = 50, height = 80, x = 100, y=100)
-        self.entry1 = tk.Text(self.win, height =1, width = 2)
         self.puppy_list = []
         # print("Bin x : "+ str(self.bin_label.coords(x)), "Bin y : "+ str(self.bin_label.coords(y)))
         self.puppy_button.pack()
@@ -30,10 +29,13 @@ class Dog:
             e.widget.destroy()
     
     def add_input_field(self, e):
+        self.entry = self.Entry(self.win)
+        
+    def move_input(self, txt):
         x = self.win.winfo_pointerx() - self.win.winfo_rootx()
         y = self.win.winfo_pointery() - self.win.winfo_rooty()
-        self.entry1.place(x=x-8 , y = y-50)
-        
+        txt.place(x=x-8 , y = y-50)
+    
 
     def delete_all(self):
         for i in self.puppy_list:
@@ -43,14 +45,18 @@ class Dog:
     def master_move(self, e):
         self.move(e)
         self.delete(e)
-        self.add_input_field(e)
+        self.move_input(self.entry.text)
 
     def create_label(self):
         self.puppy_label = tk.Label(self.win, image = self.puppy, text = str(random.randint(1,3)))
         self.puppy_label.place(height = 60, width = 50, x= random.randint(300,400), y = random.randint(300, 400))
         self.puppy_list.append(self.puppy_label)
+        self.puppy_label.bind('<Button-1>', self.add_input_field)
         self.puppy_label.bind('<B1-Motion>', self.master_move)
         return
+    class Entry:
+        def __init__(self, win):
+            self.text = tk.Text(win, height =1, width = 2)
     
     
         
